6f2e415628bfdf46c4a43c58ad51a055f4e04c4f
Oliver Franke <oliver.franke@limebit.de>
2024-02-01 11:11:04 +0100

    DIG-653 add weather history

--- src/weather_api.py
+++ src/weather_api.py
@@ -1,5 +1,4 @@
 import requests
-import json
 
 
 def get_city_coordinates(city):
@@ -10,6 +9,12 @@ def get_city_coordinates(city):
 
 def get_weather_forecast(latitude, longitude):
     url = f"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=temperature_2m_max,temperature_2m_min"
+    response = requests.get(url).json()
+    return response
+
+
+def get_weather_history(latitude, longitude, start_date, end_date):
+    url = f"https://archive-api.open-meteo.com/v1/archive?latitude={latitude}&longitude={longitude}&daily=temperature_2m_max,temperature_2m_min&start_date={start_date}&end_date={end_date}"
     response = requests.get(url).json()
     return response
 
@@ -29,12 +34,28 @@ print(
 )
 
 weather_forecast = get_weather_forecast(latitude, longitude)
-json_string = json.dumps(weather_forecast, indent=4)
-# print(json_string)
+
 
 time = weather_forecast["daily"]["time"]
 temperature_2m_max = weather_forecast["daily"]["temperature_2m_max"]
 temperature_2m_min = weather_forecast["daily"]["temperature_2m_min"]
+
+for i in range(len(time)):
+    print(
+        "At",
+        time[i],
+        "the min temperature is:",
+        temperature_2m_min[i],
+        "the max temperature is:",
+        temperature_2m_max[i],
+    )
+
+
+weather_history = get_weather_history(latitude, longitude, "2019-03-08", "2019-03-10")
+
+time = weather_history["daily"]["time"]
+temperature_2m_max = weather_history["daily"]["temperature_2m_max"]
+temperature_2m_min = weather_history["daily"]["temperature_2m_min"]
 
 for i in range(len(time)):
     print(
